a205154c87a298f4c9a0675c3e22a7f58c3ff159
CactusPuppy <coby.zhong@gmail.com>
2018-03-25 21:28:02 -0700

    Added plugin.yml and Main.java

--- src/usa/cactuspuppy/teambasedgroupfinding/Main.java
+++ src/usa/cactuspuppy/teambasedgroupfinding/Main.java
@@ -1,4 +1,22 @@
 package usa.cactuspuppy.teambasedgroupfinding;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    /**
+     * This method is called when the server loads the plugin.
+     */
+    public void onEnable() {
+
+    }
+
+
+    @Override
+    /**
+     * This method is called when the server disables the plugin.
+     */
+    public void onDisable() {
+
+    }
 }
